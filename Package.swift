2ade470aae98161a9297c14b098208ed34d91b7f
Abdelrahman Mohamed <obada.semary@gmail.com>
2024-09-19 05:08:49 +0300

    update CoreAPI

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/obadasemary/NetworkLayer.git", .upToNextMajor(from: "1.0.4"))
+        .package(path: "./NetworkLayer")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
